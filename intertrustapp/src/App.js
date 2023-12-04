7a25bb77fb590d17ec526e93f58ec2ac36ec83dc
BRUGUSOFT <kiran.paturi@brugu.io>
2023-12-04 17:08:55 +0530

    added code

--- intertrustapp/src/App.js
+++ intertrustapp/src/App.js
@@ -1,15 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { Header } from "./components/Header";
 import { headerConfig } from "./config/headerConfig";
-import { Counter } from "./components/Counter";
+import { Counter, Increment } from "./components/Counter";
 
 export const App = () => {
   const appName="InterTrust";
+  const [count,setCount]= useState(0);
+  const handleClick = (e)=>{
+    setCount(count+1);
+  }
   return (
     <div className="container-fluid">
       <Header config={headerConfig}/>
-      <Counter/>
+      <Counter count={count}/>
+      <Increment increment ={handleClick}/>
     </div>
      
   )
-}
+};
--- intertrustapp/src/components/Counter/index.js
+++ intertrustapp/src/components/Counter/index.js
@@ -1,14 +1,29 @@
 import { useState } from "react"
 
-export const Counter =()=>{
-    const [count,setCount]=useState(0);
+// export const Counter =()=>{
+//     const [count,setCount]=useState(0);
+//     const increment=(e)=>{
+//         setCount(count+1);
+//     }
+//     return(
+//         <>
+//             <h6>Count= {count}</h6>
+//             <button className="btn btn-primary" onClick={increment}>Increment</button>
+//         </>
+//     )
+// }
+
+export const Counter =(props)=>{
+    return(
+        <h6>Count= {props.count}</h6>
+    )
+}
+export const Increment =(props)=>{
+    
     const increment=(e)=>{
-        setCount(count+1);
+        props.increment(e)
     }
     return(
-        <>
-            <h6>Count= {count}</h6>
-            <button className="btn btn-primary" onClick={increment}>Increment</button>
-        </>
+        <button className="btn btn-primary" onClick={increment}></button>
     )
 }
